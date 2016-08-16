551dd7b10fff7ba3e0fd4d0a2bf8ec0db2e85974
Vladislav Balabanovich <vladislav.svoi@gmail.com>
2016-08-16 12:54:37 +0200

    Mobile adjustments

--- build/js/script.js
+++ build/js/script.js
@@ -7,11 +7,24 @@ $(document).ready(function() {
         arrows: false
     });
 
-    $('.home-slider__arrow--right').click(function(){
+    $('.home-slider__arrow--right').click(function() {
         $('#home-slider').slick("slickNext");
     });
-    $('.home-slider__arrow--left').click(function(){
+    $('.home-slider__arrow--left').click(function() {
         $('#home-slider').slick("slickPrev");
+    });
+
+
+    //Menu
+    $('.page-header__nav-toggle').on('click', function() {
+        $('.page-header__nav').fadeToggle();
+    });
+
+    $(window).resize(function(){
+        var wid = $(window).width();
+        if(wid > 768) {
+            $('.page-header__nav').show();
+        }
     });
 
 });
